481967f2eae374405cccb0347512da55807cd781
Will Martino <1953196+buckie@users.noreply.github.com>
2021-06-16 16:54:41 +0200

    added support for sending guardian commands, refactored from fretch to poll w/ backoff, refactored approach to tx status messages
    Signed-off-by: Will Martino <1953196+buckie@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Button,
   Select,
   Feed,
+  Dropdown,
   Icon,
   Message,
   Loader,
@@ -99,22 +100,29 @@ const App = () => {
   //  writes memory blockchain
   //  updates frontend depending on response
   // THIS IS FOR SIGNING API CONFIG
-  const sendGuardianCmd = async (user, cmd) => {
+  const sendGuardianCmd = async (user, cmd, envData) => {
       try {
         //creates transaction to send to wallet
         const toSign = {
             pactCode: cmd,
-            caps: `(${kadenaAPI.contractAddress}.GUARDIAN ${JSON.stringify(user)})`,
+            caps: Pact.lang.mkCap("Guadian Cap"
+                             , "Authenticates that your a guardian"
+                             , `${kadenaAPI.contractAddress}.GUARDIAN`
+                             , [user]),
             gasLimit: kadenaAPI.meta.gasLimit,
             chainId: kadenaAPI.meta.chainId,
             ttl: kadenaAPI.meta.ttl,
-            envData: {}
+            sender: user,
+            envData: envData || {}
         }
+        console.log("toSign", toSign)
         //sends transaction to wallet to sign and awaits signed transaction
         const signed = await Pact.wallet.sign(toSign)
-        console.log(signed)
+        console.log("signed", signed)
+        setTx(signed)
         //sends signed transaction to blockchain
-        const tx = await Pact.wallet.sendSigned(signed, kadenaAPI.meta.host)
+        const txReqKeys = await Pact.wallet.sendSigned(signed, kadenaAPI.meta.host)
+        console.log("txReqKeys", txReqKeys)
         //set html to wait for transaction response
         //set state to wait for transaction response
         setTxStatus('pending')
@@ -122,23 +130,42 @@ const App = () => {
           //listens to response to transaction sent
           //  note method will timeout in two minutes
           //    for lower level implementations checkout out Pact.fetch.poll() in pact-lang-api
-          let res = await Pact.fetch.listen({"listen": tx.requestKeys[0]}, kadenaAPI.meta.host);
+          let retries = 8;
+          let res = {};
+          while (retries > 0) {
+            //sleep the polling
+            await new Promise(r => setTimeout(r, 15000));
+            res = await Pact.fetch.poll(txReqKeys, kadenaAPI.meta.host);
+            try {
+              if (res[signed.hash].result.status) {
+                retries = 0;
+              } else {
+                retries = retries - 1;
+              }
+            } catch(e) {
+                retries = retries - 1;
+            }
+          };
           //keep transaction response in local state
           setTx(res)
-          if (res.result.status === "success"){
+          if (res[signed.hash].result.status === "success"){
+            console.log("tx status set to success");
             //set state for transaction success
             setTxStatus('success')
           } else {
+            console.log("tx status set to failure");
             //set state for transaction failure
             setTxStatus('failure')
           }
         } catch(e){
           console.log(e);
+          console.log("tx status set to timeout");
           //set state for transaction listening timeout
           setTxStatus('timeout')
         }
       } catch(e){
         console.log(e);
+        console.log("tx status set to validation error");
         //set state for transaction construction error
         setTxStatus('validation-error')
       }
@@ -151,30 +178,52 @@ const App = () => {
     const handleSubmit = (evt) => {
         evt.preventDefault();
         console.log(grd,newAmb,ambGrd);
-    }
+        sendGuardianCmd(grd
+          ,`(${kadenaAPI.contractAddress}.register-ambassador "${grd}" "${newAmb}" (read-keyset 'ks))`
+          ,{ks: JSON.parse(ambGrd)})
+        };
+
     return (
-      <Form onSubmit={handleSubmit}>
-        <Form.Field required>
-          <label>Guardian Account Name</label>
-          <input
-            value={grd}
-            onChange={e => setGrd(e.target.value)}/>
-        </Form.Field>
-        <Form.Field required>
-          <label>Ambassador Account Name</label>
-          <input
-            value={newAmb}
-            onChange={e => setNewAmb(e.target.value)}/>
-        </Form.Field>
-        <Form.TextArea required
-          label="Ambassador Account Guard"
-          value={ambGrd}
-          onChange={e => setAmbGrd(e.target.value)}
-          />
-        <Button
-          type='submit'
-          >Submit</Button>
-      </Form>
+      <div>
+        <Message attached header="Add Ambassador"/>
+          <Form onSubmit={handleSubmit} className="attached fluid segment">
+            <Dropdown
+              fluid
+              search
+              selection
+              placeholder="Select Guardian"
+              required
+              options={props.guardians.map((g) => {return {key: g['k'], value:g['k'], text:g['k']};})}
+              onChange={(e,d) => {setGrd(d.value)}}
+              >
+            </Dropdown>
+            <Form.Field required>
+              <label>Ambassador Account Name</label>
+              <input
+                value={newAmb}
+                onChange={e => setNewAmb(e.target.value)}/>
+            </Form.Field>
+            <Form.TextArea required
+              label="Ambassador Account Guard"
+              value={ambGrd}
+              onChange={e => setAmbGrd(e.target.value)}
+              />
+            <Button color='blue' type='submit'>Submit</Button>
+          </Form>
+          {txStatus === "pending" ? (
+            txPending()
+          ) : txStatus === "success" ? (
+            txSuccess()
+          ) : txStatus === "failure" ? (
+            txFailure()
+          ) : txStatus === "timeout" ? (
+            txTimeout()
+          ) : txStatus === "validation-error" ? (
+            txValidationError()
+          ) : (
+            <div />
+          )}
+      </div>
 
     );
   }
@@ -189,92 +238,81 @@ const App = () => {
 
   const txPending = () => {
     return (
-      <Message style={{ fontSize: 20 }}>
-        <Message.Header style={{ marginBottom: 10 }}>
-          Transaction sent to Kadena Mainnet
-        </Message.Header>
-        Please wait, this box will update once the transaction is confirmed in
-        about 30 seconds.
-        <div style={{ margin: 20, textAlign: "center" }}>
-          <Loader inline active />
-        </div>
-        <Message.Header style={{ marginBottom: 10 }}>
-          Curious how interacting with a blockchain can be so simple?
-        </Message.Header>
-        <p>
-          With most blockchains you need an account, a wallet, and some crypto
-          in order to interact with them. Kadenas solution to this onboarding
-          problem is gas stations; an account that exists only to fund gas
-          payments on behalf of other users.
-        </p>
-        <p>
-          Thanks to gas stations, interacting with Kadena can be as simple as
-          filling out a web form.
-        </p>
+      <Message icon attached="bottom" info>
+        <Icon name='circle notched' loading />
+        <Message.Content>
+          <Message.Header>
+            Transaction Sent
+          </Message.Header>
+          Awaiting Confirmation
+        </Message.Content>
       </Message>
     );
   };
-
   const txSuccess = () => {
     return (
-      <Message style={{ fontSize: 20 }}>
-        <Message.Header style={{ marginBottom: 10 }}>Success!</Message.Header>
-        <p>
-          {JSON.stringify(tx.result.data) +
-            " has been added to the Memory Wall."}
-        </p>
-        <p>
-          <a href={`${kadenaAPI.explorerURL}/tx/${tx.reqKey}`}>
+      <Message icon attached="bottom" info>
+        <Icon name='check square outline' />
+        <Message.Content>
+          <Message.Header>
+            Transaction Confirmed
+          </Message.Header>
+          <a href={`${kadenaAPI.explorerURL}/tx/${tx.hash}`}>
             View transaction in Block Explorer
           </a>
-        </p>
+        </Message.Content>
       </Message>
     );
   };
 
   const txFailure = () => {
     return (
-      <Message style={{ fontSize: 20 }}>
-        <Message.Header style={{ marginBottom: 10 }}>
-          ERROR! INVESTIGATE BELOW
-        </Message.Header>
-        <p>{JSON.stringify(tx)}</p>
-        <p>
-          <a href={`${kadenaAPI.explorerURL}/tx/${tx.reqKey}`}>
+      <Message icon attached="bottom" error>
+        <Icon name='exclaimation circle outline' />
+        <Message.Content>
+          <Message.Header>
+            Transaction Failure
+          </Message.Header>
+          <p>{JSON.stringify(tx)}</p>
+          <a href={`${kadenaAPI.explorerURL}/tx/${tx.hash}`}>
             View transaction in Block Explorer
           </a>
-        </p>
+        </Message.Content>
       </Message>
     );
   };
 
   const txValidationError = () => {
     return (
-      <Message style={{ fontSize: 20 }}>
-        <Message.Header style={{ marginBottom: 10 }}>
-          Transaction was Rejected
-        </Message.Header>
-        <p>
+      <Message icon attached="bottom" error>
+        <Icon name='exclaimation circle outline' />
+        <Message.Content>
+          <Message.Header>
+            Transaction Validation Failure
+          </Message.Header>
           Transaction was not sent to Blockchain. Check your keys or metadata
-        </p>
+        </Message.Content>
       </Message>
     );
   };
 
   const txTimeout = () => {
     return (
-      <Message style={{ fontSize: 20 }}>
-        <Message.Header style={{ marginBottom: 10 }}>
-          Waiting Timed out, but your tx was sent. Look up Below
-        </Message.Header>
-        <p>
-          <a href={`${kadenaAPI.explorerURL}/tx/${tx.reqKey}`}>
+      <Message icon attached="bottom" warning>
+        <Icon name='hourglass outline' />
+        <Message.Content>
+          <Message.Header>
+            Transaction Timeout
+          </Message.Header>
+          ...but your tx was sent.
+          <a href={`${kadenaAPI.explorerURL}/tx/${tx.hash}`}>
             View transaction in Block Explorer
           </a>
-        </p>
+        </Message.Content>
       </Message>
     );
   };
+
 
   //return react JSX
   return (
@@ -285,27 +323,44 @@ const App = () => {
             <a>Welcome to DAO.init</a>
           </h1>
 
-          <h2>Smart Contract Details:</h2>
-          <p style={{ fontSize: 18 }}>
-            <b> Address: </b> {kadenaAPI.contractAddress}
-          </p>
-          <p style={{ fontSize: 18 }}>
-            <b> Chain: </b> {kadenaAPI.meta.chainId}
-          </p>
-          <p style={{ fontSize: 18 }}>
-            <b> Network: </b> {kadenaAPI.meta.networkId}
-          </p>
-          {txStatus === "pending" ? (
-            txPending()
-          ) : txStatus === "success" ? (
-            txSuccess()
-          ) : txStatus === "timeout" ? (
-            txTimeout()
-          ) : txStatus === "validation-error" ? (
-            txValidationError()
-          ) : (
-            <div />
-          )}
+          <table className="ui very basic collapsing celled table">
+            <thead>
+              <tr>
+              </tr>
+           </thead>
+           <tbody>
+             <tr>
+               <td>
+                 <h4>
+                   Contract Address
+                 </h4>
+               </td>
+               <td>
+                  {kadenaAPI.contractAddress}
+               </td>
+             </tr>
+             <tr>
+               <td>
+                 <h4>
+                   Chain ID
+                 </h4>
+               </td>
+               <td>
+                  {kadenaAPI.meta.chainId}
+               </td>
+             </tr>
+             <tr>
+               <td>
+                 <h4>
+                   Network ID
+                 </h4>
+               </td>
+               <td>
+                  {kadenaAPI.meta.networkId}
+               </td>
+             </tr>
+           </tbody>
+          </table>
           <h2>
             Contract State
           </h2>
@@ -318,7 +373,6 @@ const App = () => {
             Ambassadors
           </h2>
           <RenderAmbassadors ambassadors={ambassadors}/>
-          <h2>Add New Ambassador</h2>
           <AddAmbassador guardians={guardians}/>
         </div>
       </Segment>
