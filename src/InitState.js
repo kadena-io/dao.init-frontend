617ad442305cd87c14b802b7df9169fdddb2900b
Will Martino <1953196+buckie@users.noreply.github.com>
2021-06-17 14:19:11 +0200

    refactored InitState to use generalized table layout
    Signed-off-by: Will Martino <1953196+buckie@users.noreply.github.com>

--- src/InitState.js
+++ src/InitState.js
@@ -3,7 +3,7 @@ import React from "react";
 //config file for blockchain calls
 import Pact from "pact-lang-api";
 import { kadenaAPI } from "./kadena-config.js";
-import { renderPactValue } from "./util.js";
+import { renderPactValue, PactSingleJsonAsTable, dashStyleNames2Text } from "./util.js";
 
 export const getContractState = async (cmd) => {
   //calling get-all() function from smart contract
@@ -30,75 +30,84 @@ export const getContractState = async (cmd) => {
 
 export const RenderInitState = (props) => {
   return (
-    <table className="ui very basic collapsing celled table">
-      <thead>
-        <tr>
-          <th></th>
-          <th>Status</th>
-        </tr>
-     </thead>
-     <tbody>
-       <tr>
-         <td>
-           <h4>
-             Guardians
-           </h4>
-         </td>
-         <td>
-            { renderPactValue( props.initState["guardian-count"]) }
-         </td>
-       </tr>
-       <tr>
-         <td>
-           <h4>
-             Ambassadors
-           </h4>
-         </td>
-         <td>
-           { renderPactValue( props.initState["ambassador-count"]) }
-         </td>
-       </tr>
-       <tr>
-         <td>
-           <h4>
-             DAO Frozen Until
-           </h4>
-         </td>
-         <td>
-          { renderPactValue( props.initState["dao-frozen-until"]) }
-         </td>
-       </tr>
-       <tr>
-         <td>
-           <h4>
-             Last Ambassador Deactivation
-           </h4>
-         </td>
-         <td>
-           { renderPactValue( props.initState["last-ambassador-deactivation"]) }
-         </td>
-       </tr>
-       <tr>
-         <td>
-           <h4>
-             Proposed Upgrade Hash
-           </h4>
-         </td>
-         <td>
-           { renderPactValue( props.initState["proposed-upgrade-hash"]) }
-         </td>
-       </tr>
-       <tr>
-         <td>
-           <h4>
-             Proposed Upgrade Time
-           </h4>
-         </td>
-         <td>
-           { renderPactValue( props.initState["proposed-upgrade-time"]) }
-         </td>
-       </tr>
-     </tbody>
-   </table>
+   <PactSingleJsonAsTable
+    json={props.initState}
+    header={["","Status"]}
+    keyFormatter={dashStyleNames2Text}
+    />
   )
 };
+
+//   return (
+//     <table className="ui very basic collapsing celled table">
+//       <thead>
+//         <tr>
+//           <th></th>
+//           <th>Status</th>
+//         </tr>
+//      </thead>
+//      <tbody>
+//        <tr>
+//          <td>
+//            <h4>
+//              Guardians
+//            </h4>
+//          </td>
+//          <td>
+//             { renderPactValue( props.initState["guardian-count"]) }
+//          </td>
+//        </tr>
+//        <tr>
+//          <td>
+//            <h4>
+//              Ambassadors
+//            </h4>
+//          </td>
+//          <td>
+//            { renderPactValue( props.initState["ambassador-count"]) }
+//          </td>
+//        </tr>
+//        <tr>
+//          <td>
+//            <h4>
+//              DAO Frozen Until
+//            </h4>
+//          </td>
+//          <td>
+//           { renderPactValue( props.initState["dao-frozen-until"]) }
+//          </td>
+//        </tr>
+//        <tr>
+//          <td>
+//            <h4>
+//              Last Ambassador Deactivation
+//            </h4>
+//          </td>
+//          <td>
+//            { renderPactValue( props.initState["last-ambassador-deactivation"]) }
+//          </td>
+//        </tr>
+//        <tr>
+//          <td>
+//            <h4>
+//              Proposed Upgrade Hash
+//            </h4>
+//          </td>
+//          <td>
+//            { renderPactValue( props.initState["proposed-upgrade-hash"]) }
+//          </td>
+//        </tr>
+//        <tr>
+//          <td>
+//            <h4>
+//              Proposed Upgrade Time
+//            </h4>
+//          </td>
+//          <td>
+//            { renderPactValue( props.initState["proposed-upgrade-time"]) }
+//          </td>
+//        </tr>
+//      </tbody>
+//    </table>
+//   )
+// };
--- src/util.js
+++ src/util.js
@@ -1,4 +1,11 @@
 // For util functions
+import React from "react";
+import {
+  Table
+} from "semantic-ui-react";
+//config file for blockchain calls
+import Pact from "pact-lang-api";
+import { kadenaAPI, keyFormatter } from "./kadena-config.js";
 
 const stringifyHelper = (k,val) => {
   return (
@@ -6,7 +13,22 @@ const stringifyHelper = (k,val) => {
   );
 }
 
+export const dashStyleNames2Text = str => str.split("-").map(k=>k.replace(new RegExp("^.","gm"),a=>a.toUpperCase())).join(' ');
+
 const pactStringify = (json) => JSON.stringify(json, (k,v) => renderPactValue(v), 2);
+
+const isRootPactValue = (val) => {
+  if (typeof val === 'object' ) {
+    if ('timep' in val || 'int' in val || 'decimal' in val || 'time' in val) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return true;
+  }
+};
+
 
 export const renderPactValue = (val) => {
   if (typeof val === 'object' ) {
@@ -30,4 +52,46 @@ export const renderPactValue = (val) => {
   } else {
     return JSON.stringify(val, undefined, 2);
   }
+};
+
+export const PactSingleJsonAsTable = (props) => {
+  const json = props.json || {};
+  const removeMargin = props.removeMargin || false;
+  const header = props.header || [];
+  const keyFormatter = props.keyFormatter ? props.keyFormatter : (k) => {return (k)};
+  const valFormatter = props.valFormatter ? props.valFormatter : (str) => <code>{renderPactValue(str)}</code>
+    return (
+      <Table simple collapsing celled style={removeMargin ? ({'margin': '0 auto', 'border-radius': '0'}) : {}}>
+        <Table.Header>
+          <Table.Row>
+          {header.map((val) => {
+            return <Table.HeaderCell>{val}</Table.HeaderCell>;
+          })}
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {Object.entries(json).map(([k,v]) => {
+            return (
+            <Table.Row>
+              <Table.Cell><h4>{keyFormatter(k)}</h4></Table.Cell>
+              { isRootPactValue(v) ? (
+                <Table.Cell>{valFormatter(v)}</Table.Cell>
+              ) : typeof v === "object" ? (
+                <PactSingleJsonAsTable
+                  json={v}
+                  keyFormatter={keyFormatter}
+                  valFormatter={valFormatter}
+                  removeMargin={true}/>
+              ) : typeof v === "function" ? (
+                <Table.Cell>{valFormatter(v.toString())}</Table.Cell>
+              ) : (
+                <Table.Cell>{valFormatter(v)}</Table.Cell>
+              )}
+            </Table.Row>
+            )
+          })}
+        </Table.Body>
+      </Table>
+    )
 };
