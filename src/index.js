56bcd7c7bbad9392015a9aca6879511be9022519
Will Martino <1953196+buckie@users.noreply.github.com>
2021-06-18 23:22:08 +0200

    pushing new gh-pages build
    Signed-off-by: Will Martino <1953196+buckie@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import NoSsr from '@material-ui/core/NoSsr';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+      <NoSsr>
         <CssBaseline/>
         <Router basename={process.env.PUBLIC_URL}>
           <App />
         </Router>
+      </NoSsr>
   </React.StrictMode>,
   document.getElementById('root')
 );
