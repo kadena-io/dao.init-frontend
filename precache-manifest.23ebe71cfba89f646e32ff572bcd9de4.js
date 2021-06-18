36ce609603ba1b7d289dd991741a4fa686752bff
Will Martino <1953196+buckie@users.noreply.github.com>
2021-06-18 22:50:46 +0200

    pushing new gh-pages build
    Signed-off-by: Will Martino <1953196+buckie@users.noreply.github.com>

---
+++ precache-manifest.23ebe71cfba89f646e32ff572bcd9de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5c0c0cfff873ada45b51780a05a9d8",
+    "url": "/dao.init-frontend/index.html"
+  },
+  {
+    "revision": "028c8177ddfdc8723966",
+    "url": "/dao.init-frontend/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "254a154de084f2b862dd",
+    "url": "/dao.init-frontend/static/js/2.fa539655.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/dao.init-frontend/static/js/2.fa539655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "028c8177ddfdc8723966",
+    "url": "/dao.init-frontend/static/js/main.a3a13a2e.chunk.js"
+  },
+  {
+    "revision": "1db12103b28cc0196cff",
+    "url": "/dao.init-frontend/static/js/runtime-main.c5e2302c.js"
+  }
+]);
