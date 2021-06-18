6f9c7ee70c4cc98ec1c0af6363174cc433000b50
Will Martino <1953196+buckie@users.noreply.github.com>
2021-06-18 22:50:46 +0200

    pushing new gh-pages build
    Signed-off-by: Will Martino <1953196+buckie@users.noreply.github.com>

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dao.init-frontend/precache-manifest.71003cf9fc3194adf58dd8acce71be3b.js"
+  "/dao.init-frontend/precache-manifest.23ebe71cfba89f646e32ff572bcd9de4.js"
 );
 
 self.addEventListener('message', (event) => {
