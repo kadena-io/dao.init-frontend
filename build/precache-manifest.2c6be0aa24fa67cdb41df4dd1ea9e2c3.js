6150b630959721f8379f6af974be84b33294ed73
Will Martino <1953196+buckie@users.noreply.github.com>
2021-07-04 16:33:00 +0200

    pushing new gh-pages build
    Signed-off-by: Will Martino <1953196+buckie@users.noreply.github.com>

--- build/precache-manifest.2c6be0aa24fa67cdb41df4dd1ea9e2c3.js
+++ build/precache-manifest.2c6be0aa24fa67cdb41df4dd1ea9e2c3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "491a5610448fa42a48ba41d6f0081830",
+    "revision": "731102de6e3d39e96571b2dd4444e2f4",
     "url": "/dao.init-frontend/index.html"
   },
   {
-    "revision": "9a61db0b1ddf1a0497ee",
+    "revision": "b62864c7177b6350417b",
     "url": "/dao.init-frontend/static/css/2.199d4a19.chunk.css"
   },
   {
-    "revision": "9a61db0b1ddf1a0497ee",
-    "url": "/dao.init-frontend/static/js/2.1b329b5f.chunk.js"
+    "revision": "b62864c7177b6350417b",
+    "url": "/dao.init-frontend/static/js/2.fbbdf8ac.chunk.js"
   },
   {
     "revision": "4f98e0a4fffcedf2e141bfb5b0e1f134",
-    "url": "/dao.init-frontend/static/js/2.1b329b5f.chunk.js.LICENSE.txt"
+    "url": "/dao.init-frontend/static/js/2.fbbdf8ac.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fc852b29ea4f0fa1e957",
-    "url": "/dao.init-frontend/static/js/main.75be263b.chunk.js"
+    "revision": "8ca9a03270c6be31b1ed",
+    "url": "/dao.init-frontend/static/js/main.e045d940.chunk.js"
   },
   {
     "revision": "1db12103b28cc0196cff",
