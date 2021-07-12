4e5f2b228425db3c17fdff422a086151409858ae
Will Martino <1953196+buckie@users.noreply.github.com>
2021-07-12 22:38:11 +0200

    working use-reducer impl
    Signed-off-by: Will Martino <1953196+buckie@users.noreply.github.com>

--- src/Wallet.js
+++ src/Wallet.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createContext, useContext, useCallback } from "react";
+import React, { useState, useEffect, createContext, useContext, useCallback, useReducer } from "react";
 import createPersistedState from 'use-persisted-state';
 import _ from 'lodash';
 
@@ -69,74 +69,68 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const walletProviderDefault = {
-    wallet: "Chainweaver",
-    signingKey: "",
-    gasPrice:"0.00000000001",
-    networkId:"testnet04",
+const walletContextDefault = {
+  current: {},
+  otherWallets: {},
+  allKeys: [],
+  contractConfigs: {},
+  pastPactTxs: [],
 };
 
-export const WalletContext = createContext({
-    walletProvider: walletProviderDefault,
-    setWalletProvider: () => {},
-    allKeys:[],
-    setAllKeys:() => {},
-    otherWallets:[],
-    setOtherWallets:() => {},
-    pastPactTxs: [],
-    setPastPactTxs: () => {},
-});
+export const WalletContext = createContext();
 
-export const useWalletProvider = (defaultObj) => {
-    const {walletProvider, setWalletProvider} = useContext(WalletContext);
-    useEffect(()=>
-        setWalletProvider({...walletProvider, ...defaultObj})
-    ,[]);
-    const mergingSetter =  useCallback((newObj) => setWalletProvider({...walletProvider, ...newObj}),[walletProvider,setWalletProvider]);
-    return [walletProvider, mergingSetter];
-};
-
-export const useSigningKeys = () => {
-    const {walletProvider, setWalletProvider} = useContext(WalletContext);
-    const allKeys = walletProvider["signingKeys"];
-    const addNewKey = useCallback((newKey) => setWalletProvider({...walletProvider, signingKeys: _.filter(_.uniq(_.concat(allKeys,newKey)), v=>v?true:false)})
-      ,[setWalletProvider,allKeys,walletProvider]);
-    return [allKeys, addNewKey];
+const walletReducer = (state, action) => {
+  switch (action.type) {
+    case 'updateWallet':
+      if (! _.has(action, "newWallet")) {throw new Error("updateWallet requires newWallet:{walletName,gasPrice,networkId,signingKey} key")};
+      const prevWallet = _.cloneDeep(state.current);
+      const otherWallets = _.cloneDeep(state.otherWallets);
+      if (prevWallet.walletName) {
+        // stops a bug on initial load where "undefined" is added as a wallet
+        otherWallets[prevWallet.walletName] = prevWallet;}
+      if (action.newWallet.walletName) {
+        otherWallets[action.newWallet.walletName] = action.newWallet;
+      } else {
+        throw new Error("updateWallet requires newWallet:{walletName,gasPrice,networkId,signingKey} key")};
+      const allKeys1 = _.filter(_.uniq(_.concat(_.cloneDeep(state.allKeys),action.newWallet.signingKey)),v=>v?true:false);
+      return {...state, current: action.newWallet, otherWallets: otherWallets, allKeys: allKeys1};
+    case 'addKeys':
+      if (! _.has(action, "newKeys")) {throw new Error("addKeys requires newKeys:[] key")};
+      const allKeys2 = _.filter(_.uniq(_.concat(_.cloneDeep(state.allKeys), action.newKeys)),v=>v?true:false);
+      return {...state, allKeys: allKeys2};
+    case 'addConfig':
+      if (! _.has(action, "configName")) {throw new Error("addConfig requires configName:'' key")};
+      if (! _.has(action, "config")) {throw new Error("addConfig requires config:'' key")};
+      const configs = _.cloneDeep(state.contractConfigs);
+      configs[action.configName] = action.config;
+      return {...state, contractConfigs:configs};
+    case 'tractPactTx':
+      if (! _.has(action, "newTx")) {throw new Error("trackPactTx requires newTx:'' key")};
+      const newPactTxs = _.concat(_.cloneDeep(state.pastPactTxs),action.newTx);
+      return {...state, pastPactTxs: newPactTxs};
+    default:
+      throw new Error(JSON.stringify(action));
+  }
 }
-
-export const usePastPactTxs = () => {
-    const {pastPactTxs, setPastPactTxs} = useContext(WalletContext);
-    const trackNewPactTx =  useCallback((newPactTx) => setPastPactTxs([...pastPactTxs, newPactTx]) ,[pastPactTxs,setPastPactTxs]);
-    return [pastPactTxs, trackNewPactTx];
-};
 
 export const Wallet = ({children}) => {
   //Wallet State
-  const [persistedWallet,setPersistedWallet] = createPersistedState("wallet")(walletProviderDefault);
-  const [walletProvider,setWalletProvider] = useState(persistedWallet);
+  const usePersistedWallet = createPersistedState("pactWallet5");
+  const [persistedWallet,setPersistedWallet] = usePersistedWallet({});
+  const [wallet,walletDispatch] = useReducer(walletReducer, _.size(persistedWallet) ? _.cloneDeep(persistedWallet) : walletContextDefault);
   // Experimental wrapper for "emit" bug found in https://github.com/donavon/use-persisted-state/issues/56
-  useEffect(()=>setPersistedWallet(walletProvider),[persistedWallet,walletProvider,setPersistedWallet]);
+  useEffect(()=>{
+    if (_.size(wallet) && ! _.isEqual(walletContextDefault, wallet)) {
+      console.log("Wallet.useEffect[persistedWallet,walletProvider,setPersistedWallet]", persistedWallet, " =to=> ", wallet);
+      setPersistedWallet(wallet);}}
+  ,[persistedWallet,wallet,setPersistedWallet]);
 
-  const [persistedAllKeys,setPersistedAllKeys] = createPersistedState("allKeys")([]);
-  const [allKeys,setAllKeys] = useState(persistedAllKeys);
-  useEffect(()=>setPersistedAllKeys(allKeys),[persistedAllKeys,allKeys,setPersistedAllKeys]);
-
-  const [persistedPactTxs,setPersistedPactTxs] = createPersistedState("pactTxs")([]);
-  const [pastPactTxs,setPastPactTxs] = useState(persistedPactTxs);
-  useEffect(()=>setPersistedPactTxs(pastPactTxs),[persistedPactTxs,pastPactTxs,setPersistedPactTxs]);
-
-  return <WalletContext.Provider value={{
-                walletProvider:walletProvider, 
-                setWalletProvider:setWalletProvider,
-                allKeys:allKeys,
-                setAllKeys:setAllKeys,
-                pastPactTxs:pastPactTxs,
-                setPastPactTxs:setPastPactTxs,
-                }}
-                >
+  return <WalletContext.Provider value={{wallet,walletDispatch}}>
           {children}
-      </WalletContext.Provider>
+         </WalletContext.Provider>
 }
+
+export const useWallet = () => useContext(WalletContext);
 
 export const walletDrawerEntries = {
   primary:"Wallet",
@@ -262,22 +256,27 @@ const walletCmd = async (
 
 const filter = createFilterOptions();
 
-const SigningKeySelect = ({signingKey, setSigningKey, allKeys}) => {
+const EntrySelector = ({
+  label,
+  getVal,
+  setVal,
+  allOpts
+}) => {
   const classes = useStyles();
-  let localOptions = allKeys ? _.clone(allKeys) : [""];
+  let localOptions = allOpts ? _.cloneDeep(allOpts) : [""];
 
   return (
     <Autocomplete
       className={classes.formControl}
-      value={signingKey}
+      value={getVal}
       onChange={(event, newValue) => {
         if (typeof newValue === 'string') {
-          setSigningKey(newValue);
+          setVal(newValue);
         } else if (newValue && newValue.inputValue) {
           // Create a new value from the user input
-          setSigningKey(newValue.inputValue)
+          setVal(newValue.inputValue)
         } else {
-          setSigningKey(newValue);
+          setVal(newValue);
         }
       }}
       filterOptions={(options, params) => {
@@ -297,7 +296,7 @@ const SigningKeySelect = ({signingKey, setSigningKey, allKeys}) => {
       fullWidth
       clearOnBlur
       handleHomeEndKeys
-      defaultValue={signingKey ? signingKey : null}
+      defaultValue={getVal ? getVal : null}
       options={localOptions}
       getOptionLabel={(option) => {
         // Value selected with enter, right from the input
@@ -312,7 +311,7 @@ const SigningKeySelect = ({signingKey, setSigningKey, allKeys}) => {
       renderOption={(option) => option.title ? option.title : option}
       freeSolo
       renderInput={(params) => (
-        <TextField {...params} label="Select Signing Key" variant="outlined" fullWidth />
+        <TextField {...params} label={label} variant="outlined" fullWidth className={classes.formControl} />
       )}
     />
   );
@@ -320,10 +319,9 @@ const SigningKeySelect = ({signingKey, setSigningKey, allKeys}) => {
 
 
 export const WalletConfig = () => {
-  const [walletProvider, setWalletProvider] = useWalletProvider();
-  const [allKeys, addNewKey] = useSigningKeys();
+  const {wallet, walletDispatch} = useContext(WalletContext);
   const [saved,setSaved] = useState(false);
-  const [wallet,setWallet] = useState("Chainweaver");
+  const [walletName,setWalletName] = useState("Chainweaver");
   const [signingKey, setSigningKey] = useState("");
   const [networkId, setNetworkId] = useState("testnet04");
   const [gasPrice, setGasPrice] = useState("");
@@ -334,17 +332,30 @@ export const WalletConfig = () => {
   const [txRes, setTxRes] = useState({});
 
   useEffect(()=> {
-    console.log("WalletConfig useEffect on load fired with", walletProvider)
-    if (_.size(walletProvider)) {
-        if (walletProvider.wallet) {setWallet(walletProvider.wallet)}
-        if (walletProvider.signingKey) {setSigningKey(walletProvider.signingKey)}
-        if (walletProvider.gasPrice) {setGasPrice(walletProvider.gasPrice)}
-        if (walletProvider.networkId) {setNetworkId(walletProvider.networkId)}
+    console.log("WalletConfig useEffect on load fired with", wallet)
+    if (_.size(wallet.current)) {
+        if (wallet.current.walletName) {setWalletName(wallet.current.walletName)}
+        if (wallet.current.signingKey) {setSigningKey(wallet.current.signingKey)}
+        if (wallet.current.gasPrice) {setGasPrice(wallet.current.gasPrice)}
+        if (wallet.current.networkId) {setNetworkId(wallet.current.networkId)}
     } 
   }
   ,[]);
+  
+  useEffect(()=>{
+    if (_.size(wallet.otherWallets[walletName])) {
+      const loadingWallet = wallet.otherWallets[walletName];
+      console.debug("WalletConfig updating entries", loadingWallet)
+        if (loadingWallet.walletName && loadingWallet.signingKey && loadingWallet.gasPrice && loadingWallet.networkId) {
+          setGasPrice(loadingWallet.gasPrice);
+          setSigningKey(loadingWallet.signingKey);
+          setNetworkId(loadingWallet.networkId);
+          setWalletName(loadingWallet.walletName);
+        }
+    }
+  },[walletName])
 
-  useEffect(()=>setSaved(false),[wallet,signingKey,gasPrice,networkId]);
+  useEffect(()=>setSaved(false),[walletName,signingKey,gasPrice,networkId]);
 
   const handleSubmit = (evt) => {
       evt.preventDefault();
@@ -362,29 +373,28 @@ export const WalletConfig = () => {
           Number.parseFloat(gasPrice), 
           host);
       } else {
-        const n = {wallet:wallet, signingKey:signingKey, gasPrice:gasPrice, networkId:networkId};
-        setWalletProvider(n);
-        addNewKey(signingKey);
+        const n = {walletName:walletName, signingKey:signingKey, gasPrice:gasPrice, networkId:networkId};
+        walletDispatch({type: 'updateWallet', newWallet: n});
         setSaved(true);
-        console.log("WalletConfig set to", n)
+        console.log("WalletConfig set. locale: ", n, " while context is: ", wallet.current);
       }
   };
   const inputFields = [
     {
-      type:'select',
-      label:'Wallet',
-      className:classes.formControl,
-      onChange:setWallet,
-      value:wallet,
-      options:["Chainweaver", "Zelcore"],
-    },{
-      type:'select',
-      label:'Network',
-      className:classes.formControl,
-      onChange:setNetworkId,
-      value:networkId,
-      options:["testnet04", "mainnet01"],
-    },{
+    //   type:'select',
+    //   label:'Wallet',
+    //   className:classes.formControl,
+    //   onChange:setWalletName,
+    //   value:walletName,
+    //   options:["Chainweaver", "Zelcore"],
+    // },{
+    //   type:'select',
+    //   label:'Network',
+    //   className:classes.formControl,
+    //   onChange:setNetworkId,
+    //   value:networkId,
+    //   options:["testnet04", "mainnet01"],
+    // },{
       type:'textFieldSingle',
       label:'Gas Price',
       className:classes.formControl,
@@ -396,10 +406,12 @@ export const WalletConfig = () => {
       <form
         autoComplete="off"
         onSubmit={(evt) => handleSubmit(evt)}>
+        <EntrySelector label="Wallet Name" getVal={walletName} setVal={setWalletName} allOpts={_.keys(wallet.otherWallets)}/>
+        <EntrySelector label="Network ID" getVal={networkId} setVal={setNetworkId} allOpts={_.keys(wallet.otherWallets, v=>v.networkId)}/>
         {inputFields.map(f =>
           <MakeInputField inputField={f}/>
         )}
-        <SigningKeySelect signingKey={signingKey} setSigningKey={setSigningKey} allKeys={allKeys}/>
+        <EntrySelector label="Select Signing Key" getVal={signingKey} setVal={setSigningKey} allOpts={wallet.allKeys}/>
         <CardActions>
             <Button variant="outlined" color="default" type="submit">
                 {saved ? "Test Current Settings" : "Save New Settings" }
